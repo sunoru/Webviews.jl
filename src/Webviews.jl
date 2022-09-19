fbae42a31666f8d8c123787bc2b55a6a35c202a0
スノル <s@sunoru.com>
2022-09-19 22:39:18 +0000

    Add an example.

--- src/Webviews.jl
+++ src/Webviews.jl
@@ -1,15 +1,18 @@
 # Wrappers for https://github.com/webview/webview
-module WebViews
+module Webviews
 
-export WebView,
-    terminate,
+using JSON
+
+export Webview,
+    terminate!,
     window_handle,
     title!,
     navigate!,
     html!,
     init!,
     eval!,
-    bind
+    bind!,
+    unbind!
 
 export WindowSizeHint,
     WEBVIEW_HINT_NONE,
@@ -19,9 +22,10 @@ export WindowSizeHint,
 
 const libwebview = joinpath(@__DIR__, "..", "build", "libwebview.so")
 
-mutable struct WebView
+mutable struct Webview
     const handle::Ptr{Cvoid}
-    WebView(; debug=false) =
+    const callbacks::Dict{String,Base.CFunction}
+    Webview(; debug=false) =
         finalizer(new(
             ccall(
                 (:webview_create, libwebview),
@@ -29,7 +33,8 @@ mutable struct WebView
                 (Cint, Ptr{Cvoid}),
                 debug, C_NULL
             )
-        )) do w
+        , Dict())) do w
+            # TODO: unbind callbacks
             ccall(
                 (:webview_destroy, libwebview),
                 Cvoid,
@@ -38,28 +43,28 @@ mutable struct WebView
             )
         end
 end
-Base.cconvert(::Type{Ptr{Cvoid}}, w::WebView) = w.handle
+Base.cconvert(::Type{Ptr{Cvoid}}, w::Webview) = w.handle
 
-Base.run(w::WebView) = ccall(
+Base.run(w::Webview) = ccall(
     (:webview_run, libwebview),
     Cvoid,
     (Ptr{Cvoid},),
     w
 )
-terminate(w::WebView) = ccall(
+terminate!(w::Webview) = ccall(
     (:webview_terminate, libwebview),
     Cvoid,
     (Ptr{Cvoid},),
     w
 )
 # webview_dispatch(w, fn, arg)
-window_handle(w::WebView) = ccall(
+window_handle(w::Webview) = ccall(
     (:webview_get_window, libwebview),
     Ptr{Cvoid},
     (Ptr{Cvoid},),
     w
 )
-title!(w::WebView, title::AbstractString) = ccall(
+title!(w::Webview, title::AbstractString) = ccall(
     (:webview_set_title, libwebview),
     Cvoid,
     (Ptr{Cvoid}, Cstring),
@@ -73,46 +78,71 @@ title!(w::WebView, title::AbstractString) = ccall(
     WEBVIEW_HINT_FIXED = 3 # Window size can not be changed by a user
 end
 
-navigate!(w::WebView, url::AbstractString) = ccall(
+navigate!(w::Webview, url::AbstractString) = (ccall(
     (:webview_navigate, libwebview),
     Cvoid,
     (Ptr{Cvoid}, Cstring),
     w, url
-)
-html!(w::WebView, html::AbstractString) = ccall(
+); w)
+html!(w::Webview, html::AbstractString) = (ccall(
     (:webview_set_html, libwebview),
     Cvoid,
     (Ptr{Cvoid}, Cstring),
     w, html
-)
-init!(w::WebView, js::AbstractString) = ccall(
+); w)
+init!(w::Webview, js::AbstractString) = (ccall(
     (:webview_init, libwebview),
     Cvoid,
     (Ptr{Cvoid}, Cstring),
     w, js
-)
-eval!(w::WebView, js::AbstractString) = ccall(
+); w)
+eval!(w::Webview, js::AbstractString) = (ccall(
     (:webview_eval, libwebview),
     Cvoid,
     (Ptr{Cvoid}, Cstring),
     w, js
-)
+); w)
 
-function _raw_bind(@nospecialize(f::Function), w::WebView, name::AbstractString)
-    cf = @cfunction $f Cvoid (Ptr{Cvoid}, Ptr{Cvoid}, Ptr{Cvoid})
+function _raw_bind(@nospecialize(f::Function), w::Webview, name::AbstractString)
+    cf = @cfunction $f Cvoid (Ptr{Cchar}, Ptr{Cchar}, Ptr{Cvoid})
     ccall(
         (:webview_bind, libwebview),
         Cvoid,
         (Ptr{Cvoid}, Cstring, Ptr{Cvoid}, Ptr{Cvoid}),
         w, name, cf, C_NULL
     )
+    cf
 end
-function bind(@nospecialize(f::Function), w::WebView, name::AbstractString)
-    function wrapper(seq_ptr::Ptr{Cvoid}, req_ptr::Ptr{Cvoid}, ::Ptr{Cvoid})
+_return(w::Webview, seq_ptr::Ptr{Cchar}, success::Bool, result) = ccall(
+    (:webview_return, libwebview),
+    Cvoid,
+    (Ptr{Cvoid}, Ptr{Cchar}, Cint, Cstring),
+    w, seq_ptr, !success, JSON.json(result)
+)
+function bind!(f::Function, w::Webview, name::AbstractString)
+    function wrapper(seq_ptr::Ptr{Cchar}, req_ptr::Ptr{Cchar}, ::Ptr{Cvoid})
         req = unsafe_string(req_ptr)
         args = JSON.parse(req)
+        try
+            result = f(args...)
+            _return(w, seq_ptr, true, result)
+        catch err
+            _return(w, seq_ptr, false, err)
+        end
     end
-    _raw_bind(wrapper, w, name)
+    cf = _raw_bind(wrapper, w, name)
+    w.callbacks[name] = cf
+    w
+end
+function unbind!(w::Webview, name::AbstractString)
+    ccall(
+        (:webview_unbind, libwebview),
+        Cvoid,
+        (Ptr{Cvoid}, Cstring),
+        w, name
+    )
+    delete!(w.callbacks, name)
+    w
 end
 
 end # module
