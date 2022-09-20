b329b9a362e5a00e372038eb40384dfec0c882fe
スノル <s@sunoru.com>
2022-09-20 06:41:50 +0000

    Add artifats.

---
+++ test/runtests.jl
@@ -0,0 +1,33 @@
+using Test
+using Webviews
+
+@testset "Webviews.jl" begin
+    webview = Webview(
+        title="Test",
+        debug=true,
+        size_hint=WEBVIEW_HINT_MAX
+    )
+    resize!(webview, (320, 240))
+    @test webview.size == (320, 240)
+    sizehint!(webview, WEBVIEW_HINT_MAX)
+    @test webview.size_hint == WEBVIEW_HINT_MAX
+    @test window_handle(webview) != C_NULL
+    html = """<html><body><h1>Hello from Julia v$VERSION</h1></body></html>"""
+    step = 0
+    bind!(webview, "run_test") do
+        @show step += 1
+        if step == 1
+            html!(webview, html)
+        elseif step == 2
+            eval!(webview, "run_test()")
+        elseif step == 3
+            terminate(webview)
+        end
+        nothing
+    end
+    init!(webview, "run_test().catch(console.error)")
+    navigate!(webview, "data:text/html,$html")
+    run(webview)
+    webview = nothing
+    GC.gc()
+end
